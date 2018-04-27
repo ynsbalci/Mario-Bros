a7b14f962887df71e6509eabcd6ed47fbf267c1b
Hsn07 <hbacak15@posta.pau.edu.tr>
2018-04-27 12:18:26 +0300

    Yorum satırları
    Yorum satırları eklendi

--- src/com/hay/mariobros/Keyboard.java
+++ src/com/hay/mariobros/Keyboard.java
@@ -6,11 +6,11 @@ import java.awt.event.KeyListener;
 public  class Keyboard implements KeyListener {
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+	public void keyPressed(KeyEvent e) {//Herhangi bir tuþa basýldýðý zaman tuþlarýn iþlevlerine  göre gerekli iþlemlerin atanmasý
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {//sað tuþuna basýldýðý zaman yapýlacaklar
 			Main.scene.setDx(1);
 			
-		}else if(e.getKeyCode()==KeyEvent.VK_LEFT)
+		}else if(e.getKeyCode()==KeyEvent.VK_LEFT)//sol tuþuna basýldðý zaman yapýlacaklar
 		{
 			Main.scene.setDx(-1);
 		}
@@ -18,14 +18,15 @@ public  class Keyboard implements KeyListener {
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased(KeyEvent e) {//elimizi tustan cektigimiz anda calisan olaydir
+		
 		Main.scene.setDx(0);
 		
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
-		
+	public void keyTyped(KeyEvent e) {//tuþa basýlý olduðu müddetçee çalýþan olaydýr
+		System.out.print("typed");
 	}
 	
 	
