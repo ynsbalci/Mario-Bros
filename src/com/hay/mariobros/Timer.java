fe3eccf8925103a60e896eb23307a08d5685d506
YunusBALCI <ynsbalci@outlook.com>
2018-04-27 10:47:35 +0300

    Added Timer
    Timer implemented Runnabel and writed run function

--- src/com/hay/mariobros/Timer.java
+++ src/com/hay/mariobros/Timer.java
@@ -1,5 +1,21 @@
 package com.hay.mariobros;
 
-public class Timer {
+public class Timer implements Runnable {
+	
+	private final int PAUSE = 3;
+	
+	@Override
+	public void run() {
+		while (true) {
+			Main.scene.repaint();
+			try {
+				Thread.sleep(PAUSE);
+			} catch (InterruptedException e) {
+				// TODO: handle exception
+			}
+			
+		}
+	}
 
+	
 }
