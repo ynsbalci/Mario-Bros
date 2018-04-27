8d0d77ec42e6835ff8c3b5a1f39153649059bd2b
Hsn07 <hbacak15@posta.pau.edu.tr>
2018-04-27 12:02:45 +0300

    KeyBoard Claassı
    Keyboard classı KeyListener ile implement edildikten sonra
    public void keyPressed, keyReleased, keyTyped metotları eklendi...

--- src/com/hay/mariobros/Keyboard.java
+++ src/com/hay/mariobros/Keyboard.java
@@ -1,5 +1,33 @@
 package com.hay.mariobros;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Keyboard {
+
+public  class Keyboard implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			Main.scene.setDx(1);
+			
+		}else if(e.getKeyCode()==KeyEvent.VK_LEFT)
+		{
+			Main.scene.setDx(-1);
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		Main.scene.setDx(0);
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	
+	
 
 }
