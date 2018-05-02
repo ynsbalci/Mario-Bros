1b6a15dc494a4c30beab63106d7a84e28323d0dc
hsn07 <hsn072014@hotmail.com>
2018-05-02 23:15:22 +0300

    Scene Classı
    Scene classında düzeltmeler yapıldı

--- src/com/hay/mariobros/Scene.java
+++ src/com/hay/mariobros/Scene.java
@@ -13,7 +13,7 @@ import com.hay.objects.Tube;
 
 @SuppressWarnings("serial")
 public class Scene extends JPanel{
-	
+	//*****VARIABLES*****//
 	private ImageIcon icoBg;
 	private Image imgBg1;
 	private Image imgBg2;
@@ -25,6 +25,9 @@ public class Scene extends JPanel{
 	private int xBg2;
 	private int dx;
 	private int xPos;
+	private int ysol;
+    private int ceilingheater=0;
+    	
 	public Mario mario;
 	public Tube tube;
 	public Block block;
@@ -33,8 +36,8 @@ public class Scene extends JPanel{
 	public Scene(){
 		super();
 		
-		this.xBg1 = 0;
-		this.xBg2 = 0;
+		this.xBg1 = -50;
+		this.xBg2 = 750;
 		this.dx = 0;
 		this.xPos = -1;
 		
@@ -63,7 +66,9 @@ public class Scene extends JPanel{
 	//**** GETTERS ****//
 	public int getDx() {return dx;}
 	public int getxPos() {return xPos;}
-	
+	public int getYsol() {return ysol;}
+	public int getCeilingheater() {return ceilingheater;}
+
 	//**** SETTERS ****//
 	public void setDx(int dx) {this.dx = dx;}
 	public void setxPos(int xPos) {this.xPos = xPos;}
@@ -92,22 +97,29 @@ public class Scene extends JPanel{
 		this.displacementBg();
 		
 		if (true) {
+			this.mario.setMovement(false);
+			this.dx=0;
 			//marionun herhangi bir engele çarptı mı diye bakıcaz
 			//eğer çarptı ise movemet false olcak
 			//dx i sıfırla
 		}
 		this.displacementBg();
-		//this.tube.displacement;
+		this.tube.displacement();
 		
 		g2.drawImage(this.imgBg1, this.xBg1, 0, null);
 		g2.drawImage(this.imgBg2, this.xBg2, 0, null);
-		g2.drawImage(this.mario.move("mario", 25), 300, 245, null);
+
 		g2.drawImage(this.imgCastleStart,  10-this.xPos, 95, null);
 		g2.drawImage(this.imgStart, 220-this.xPos, 234, null);
+		
 		g2.drawImage(this.tube.getImgTube(), this.tube.getX(), this.tube.getY(), null);
 		g2.drawImage(this.block.getImgBlock(), this.block.getX() - this.xPos, this.block.getY(), null);
-				
-		
+		if(this.mario.isMovement()) {
+			//g2.drawImage(this.mario.isMovement(), this.mario.getX(), this.mario.getY(), null);
+					
+		}else {
+			g2.drawImage(this.mario.move("mario", 25), this.mario.getX(), this.mario.getY(), null);
+		}
 	}
 
 }
