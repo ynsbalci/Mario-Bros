ec752d8483195b0c17ee7d7e448cc31f2a2b735e
hsn07 <hsn072014@hotmail.com>
2018-05-04 21:30:42 +0300

    Block ve Tube Güncellendi.

--- src/com/hay/objects/Block.java
+++ src/com/hay/objects/Block.java
@@ -6,7 +6,7 @@ public class Block extends Object {
 	
 	//***** CONSTRUCTER *****//
 	public Block(int x, int y) {
-		super(x, y, 50, 50);
+		super(x, y, 30, 30);
 		super.icoObjects = new ImageIcon(getClass().getResource("/images/block.png"));
 		super.imgObjects = this.icoObjects.getImage();
 	}
