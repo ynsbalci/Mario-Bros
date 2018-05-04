64cb05047f746965d653eca10969e30375cb5060
YunusBALCI <ynsbalci@outlook.com>
2018-05-04 12:12:58 +0300

    ok

--- src/com/hay/objects/Cloud.java
+++ src/com/hay/objects/Cloud.java
@@ -1,22 +1,15 @@
 package com.hay.objects;
 
-<<<<<<< HEAD
+import javax.swing.ImageIcon;
+
+
 public class Cloud extends Object{
 
 	public Cloud(int x, int y, int width, int height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
-=======
-import javax.swing.ImageIcon;
-
-public class Cloud extends Object {
-	
-	//***** CONSTRUCTER *****//
-	public Cloud(int x, int y) {	
-		super(x, y, 100, 50);
 		super.icoObjects = new ImageIcon(getClass().getResource("/images/cloud_1.png"));
 		super.imgObjects = this.icoObjects.getImage();
->>>>>>> 3062574405faa82a2fee37c0fb648ffec3d2c681
+
 	}
 
 }
